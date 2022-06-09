c930309ec6772245a8ceb8b667b5241ed8516ffd
ilyashirko <ilyashirko@gmail.com>
2022-06-09 11:14:23 +0300

    count most popular posts

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from blog.models import Comment, Post, Tag
+from django.db.models import Count
 
 
 def get_related_posts_count(tag):
@@ -27,9 +28,14 @@ def serialize_tag(tag):
     }
 
 
-def index(request):
+def get_likes_count(post):
+    return post.likes.all().count()
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+
+def index(request):
+    most_popular_posts = Post.objects.annotate(
+        count_likes=Count('likes')
+    ).order_by('-count_likes')[:5]
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
