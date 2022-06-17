270ba0d2bab6b61054dea58c70b4300a85c6fb55
ilyashirko <ilyashirko@gmail.com>
2022-06-17 16:23:54 +0300

    optimized CPU time

--- blog/models.py
+++ blog/models.py
@@ -5,11 +5,6 @@ from django.urls import reverse
 
 
 class PostManager(models.QuerySet):
-    def year(self, year):
-        return self.filter(
-            published_at__year=year
-        ).order_by('published_at')
-
     def popular(self):
         return (
             self.prefetch_related('author', 'tags')
@@ -17,11 +12,24 @@ class PostManager(models.QuerySet):
                 .order_by('-count_likes')
         )
 
+    def fresh(self):
+        return (
+            self.prefetch_related('author', 'tags')
+                .order_by('-published_at')
+        )
+
     def fetch_with_comments_count(self):
-        self_with_comments = self.prefetch_related('comments')
-        for post in self_with_comments:
-            post.count_comments = post.comments.count()
-        return self_with_comments
+        posts_ids = self.values_list("id", flat=True)
+        posts_with_comments = Post.objects.filter(id__in=posts_ids) \
+                                          .annotate(
+                                            count_comments=Count("comments")
+                                          )
+        ids_and_comments = dict(
+            posts_with_comments.values_list("id", "count_comments")
+        )
+        for post in self:
+            post.count_comments = ids_and_comments[post.id]
+        return self
 
 
 class TagManager(models.QuerySet):
